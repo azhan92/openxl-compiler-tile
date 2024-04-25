d77d3a292a504b2acc2d130013dba6b4af38e1cb
azhan92 <alisonxzhang@gmail.com>
2024-04-25 15:24:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,10 +23,11 @@ resource "ibm_pi_image" "rhcos_image_import" {
   pi_image_bucket_name      = "open-xl-bucket"
   pi_image_bucket_region    = "us-south"
   pi_image_bucket_file_name = "open-xl-image.ova.gz"
+  pi_image_storage_type     = "tier3"
 }
 
 data "ibm_pi_image" "rhcos" { 
-  pi_image_name        = var.rhcos_image_name
+  pi_image_name        = ibm_pi_image.rhcos_image_import[0].pi_image_name
   pi_cloud_instance_id = var.service_instance_id
 }
 
@@ -36,7 +37,7 @@ resource "ibm_pi_instance" "instance" {
   pi_processors        = var.processors
   pi_instance_name     = var.instance_name
   pi_proc_type         = var.processor_type
-  pi_image_id          = ibm_pi_image.rhcos_image_import.image_id
+  pi_image_id          = ibm_pi_image.rhcos_image_import[0].image_id
   pi_key_pair_name     = data.ibm_pi_key.key.id
   pi_sys_type          = var.sys_type
   pi_storage_type      = var.storage_type
