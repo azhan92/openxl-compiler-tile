ea793518d2434dd56f0e649cf314d463120c56bd
azhan92 <alisonxzhang@gmail.com>
2024-04-29 11:36:50 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,25 @@
+data "ibm_pi_catalog_images" "catalog_images" {
+  sap                  = true
+  vtl                  = true
+  pi_cloud_instance_id = local.pid
+}
+
+data "ibm_pi_images" "cloud_instance_images" {
+  pi_cloud_instance_id = local.pid
+}
+
 locals {
+  stock_image_name = "openxltile-v17-01-01"  
+  catalog_image = [for x in data.ibm_pi_catalog_images.catalog_images.images : x if x.name == local.stock_image_name]
+  private_image = [for x in data.ibm_pi_images.cloud_instance_images.image_info : x if x.name == local.stock_image_name]
+  private_image_id = length(local.private_image) > 0 ? local.private_image[0].id  : ""
   placement_group = [for x in data.ibm_pi_placement_groups.cloud_instance_groups.placement_groups : x if x.name == var.placement_group]
   placement_group_id = length(local.placement_group) > 0 ? local.placement_group[0].id : ""
 }
 
 data "ibm_pi_placement_groups" "cloud_instance_groups" {
   pi_cloud_instance_id = local.pid
-} 
+}    
 
 data "ibm_pi_key" "key" {
   pi_cloud_instance_id = local.pid
@@ -16,20 +30,13 @@ data "ibm_pi_network" "power_network" {
   pi_cloud_instance_id = local.pid
   pi_network_name      = var.network_name
 }
-    
-resource "ibm_pi_image" "rhcos_image_import" {
-  pi_image_name             = var.rhcos_image_name
-  pi_cloud_instance_id      = var.service_instance_id
-  pi_image_bucket_name      = "open-xl-bucket"
-  pi_image_bucket_region    = "us-south"
-  pi_image_bucket_file_name = "open-xl-image.ova.gz"
-  pi_image_storage_type     = "tier3"
-  pi_image_bucket_access    = "public"
-}
 
-data "ibm_pi_image" "rhcos" { 
-  pi_image_name        = ibm_pi_image.rhcos_image_import.pi_image_name
-  pi_cloud_instance_id = var.service_instance_id_2
+resource "ibm_pi_image" "stock_image_copy" {
+  count = length(local.private_image_id) == 0 ? 1 : 0
+
+  pi_image_name       = local.stock_image_name
+  pi_image_id         = local.catalog_image[0].image_id
+  pi_cloud_instance_id = var.service_instance_id
 }
 
 resource "ibm_pi_instance" "instance" {
@@ -38,7 +45,7 @@ resource "ibm_pi_instance" "instance" {
   pi_processors        = var.processors
   pi_instance_name     = var.instance_name
   pi_proc_type         = var.processor_type
-  pi_image_id          = ibm_pi_image.rhcos_image_import.image_id
+  pi_image_id          = length(local.private_image_id) == 0 ? ibm_pi_image.stock_image_copy[0].image_id : local.private_image_id
   pi_key_pair_name     = data.ibm_pi_key.key.id
   pi_sys_type          = var.sys_type
   pi_storage_type      = var.storage_type
