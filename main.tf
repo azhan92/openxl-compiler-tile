816a1e285dcb785a04a5c1f93fd9bf083bd59687
azhan92 <alisonxzhang@gmail.com>
2024-04-29 13:12:05 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -40,7 +40,7 @@ resource "ibm_pi_instance" "instance" {
   pi_processors        = var.processors
   pi_instance_name     = var.instance_name
   pi_proc_type         = var.processor_type
-  pi_image_id          = ibm_pi_image.rhcos_image_import.pi_image_id
+  pi_image_id          = ibm_pi_image.rhcos_image_import.image_id
   pi_key_pair_name     = data.ibm_pi_key.key.id
   pi_sys_type          = var.sys_type
   pi_storage_type      = var.storage_type
