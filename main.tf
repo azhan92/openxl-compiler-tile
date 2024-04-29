97f040137756c28406591300f49c82acdeddc03f
azhan92 <alisonxzhang@gmail.com>
2024-04-29 12:29:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,16 +20,6 @@ data "ibm_pi_network" "power_network" {
   pi_network_name      = var.network_name
 }
 
-resource "ibm_pi_image" "rhcos_image_import" {
-  pi_image_name             = var.rhcos_image_name
-  pi_cloud_instance_id      = var.service_instance_id
-  pi_image_bucket_name      = "open-xl-bucket"
-  pi_image_bucket_region    = "us-south"
-  pi_image_bucket_file_name = "open-xl-image.ova.gz"
-  pi_image_storage_type     = "tier3"
-  pi_image_bucket_access    = "public"
-}
-
 data "ibm_pi_catalog_images" "catalog_images" {
   sap                  = true
   vtl                  = true
